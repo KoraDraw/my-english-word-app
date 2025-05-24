2dd3e35344214e973955421a2cef744f406c6976
KoraDraw <korinachernysheva@mail.ru>
2025-05-24 20:55:02 +0300

    Описание внесённых изменений

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import WordPage from "./components/pages/WordPage";
 import MainPage from "./components/pages/MainPage";
+import CarouselPage from "./components/pages/CarouselPage";
 
 function App() {
   return (
     <div className="App">
       <WordPage />
       <MainPage />
+      <CarouselPage />
     </div>
   );
 }
