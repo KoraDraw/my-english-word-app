d848fae3a6c5602a225b4588c27c24e2d18af728
KoraDraw <korinachernysheva@mail.ru>
2025-05-30 17:47:26 +0300

    add missing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar/NavBar";
 import WordPage from "./components/pages/WordPage";
 import MainPage from "./components/pages/MainPage";
 import CarouselPage from "./components/pages/CarouselPage";
+import Missing from "./components/Missing/Missing";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/words" element={<WordPage />} />
           <Route path="/carousel" element={<CarouselPage />} />
+          <Route path="/error" element={<Missing />} />
         </Routes>
       </div>
     </Router>
