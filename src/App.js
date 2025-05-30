e9c2fe170c7cf705347855e5e25af60c35aea6d7
KoraDraw <korinachernysheva@mail.ru>
2025-05-30 17:05:10 +0300

    add navBar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import "./App.css";
 import WordPage from "./components/pages/WordPage";
 import MainPage from "./components/pages/MainPage";
 import CarouselPage from "./components/pages/CarouselPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <WordPage />
-      <MainPage />
-      <CarouselPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/words" element={<WordPage />} />
+          <Route path="/carousel" element={<CarouselPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+function NavBar({ title = "English words" }) {
+  return (
+    <div>
+      <h1>Menu</h1>
+    </div>
+  );
+}
+export default NavBar;
